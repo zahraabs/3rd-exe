6593b57b1f16cb04a17b905bcb9daed8d87ec24d
zahraabs <zahraabbasiun@gmail.com>
2023-08-18 10:29:16 +0330

    one validation for empty added

--- js/main.js
+++ js/main.js
@@ -4,27 +4,54 @@ var btn = document.querySelector(".button");
 var list = document.querySelector("ul");
 var inpSearch = document.querySelector("#search");
 var myArray = [];
+var exist =false;
 var searchArray = [];
 
 // make addEventListener function for clicking on add button
 btn.addEventListener("click", function () {
     // make variable for trim input text
     var inpTrim = inpText.value.trim();
-    var inpTrimLower = inpTrim.toLowerCase();
-    // if existing input value in my array is true make it null
-    if (exist(myArray, inpTrimLower)) {
-        inpText.value = null;
-        alert("Your item is existed! Enter new...");
-        // then if it is not empty
-    } else if (inpText.value != "") {
-        // push inpTrim in myArray
-        pushArray(myArray, inpTrimLower);
-        // for showing list in output
-        list.style.display = "block";
-        // for adding items in the list
+    // if myArray is empty
+    if (myArray.length == 0) {
+          // push inptrim in myArraySearch
+        pushArray(myArray, inpTrim);
+        // add inptrim in listInner
         listInner(inpTrim);
-        // empty the inpText for writing new item
+        // show list in output
+        list.style.display = "block";
+        // make inpText empty
         inpText.value = null;
+        // if myArray is not empty
+    } else {
+        // condition for inpTrim existing in array
+        for (var i = 0; i < myArray.length; i++) {
+            // if exist is true go out from loop
+            if (inpTrim.toLowerCase() == myArray[i].toLowerCase()) {
+                exist =true;
+                i=myArray.length;
+
+            }else{
+                exist =false;
+            }
+         
+        }
+        // condition for existing in the array
+        if  (exist){
+            inpText.value = null;
+            // show alert for existed item
+            alert("Your item is existed! Enter new...");
+          }
+          else {          
+            // push inpTrim in myArray for duplicate words
+            pushArray(myArray, inpTrim);
+            // for showing list in output
+            list.style.display = "block";
+            // for adding items in the list
+            listInner(inpTrim);
+            // empty the inpText for writing new item
+            inpText.value = null;
+        }
+    
     }
 });
 
@@ -39,9 +66,9 @@ inpSearch.addEventListener("keyup", function () {
         for (var i = 0; i < myArray.length; i++) {
             if (searchWith(myArray[i], inpSearch.value)) {
                 // push indexes of myArray in searchArray
-                pushArray(searchArray, myArray[i])
+                pushArray(searchArray, myArray[i]);
                 // show the list of searching
-                listInner(myArray[i])
+                listInner(myArray[i]);
             }
         }
         // else if it is empty
@@ -55,10 +82,6 @@ inpSearch.addEventListener("keyup", function () {
     }
 });
 
-// function for including value in the array
-function exist(arrayIndex, value) {
-    return arrayIndex.includes(value);
-}
 
 // function for pushing one array in another
 function pushArray(firstArr, secondArr) {
@@ -72,8 +95,8 @@ function searchWith(arrayIndex, value) {
 
 // function for making list in inner html
 function listInner(value) {
-    if (value == undefined) {
-        return list.innerHTML = [];
+    if (value=="" || value == undefined ) {
+        return list.innerHTML += [];
     } else {
         return list.innerHTML += `<li>${value}</li>`;
     }
